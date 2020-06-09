31955909be7d878b948de3adc1a4ec1816e8b165
Adam Dangoor <adamdangoor@gmail.com>
2020-06-09 23:58:52 +0100

    Get to new page

--- script.py
+++ script.py
@@ -1,8 +1,12 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Safari()
+ten_second_wait = WebDriverWait(driver, 10)
 log_in_url = 'https://developer.vuforia.com/vui/auth/login'
 licenses_url = 'https://developer.vuforia.com/vui/develop/licenses'
 email_address = os.environ['EMAIL_ADDRESS']
@@ -15,7 +19,12 @@ password_input_element = driver.find_element_by_id('login_password')
 password_input_element.send_keys(password)
 password_input_element.send_keys(Keys.RETURN)
 
-get_development_key_button_element = driver.find_element_by_id('get-development-key')
+get_development_key_button_element = ten_second_wait.until(
+    expected_conditions.presence_of_element_located(
+        (By.ID, 'get-development-key'),
+    ),
+)
 
+get_development_key_button_element.click()
 import pdb; pdb.set_trace()
 driver.close()
