cd701a2e229c847080c66752778c9c58e951fed2
Adam Dangoor <adamdangoor@gmail.com>
2020-06-10 10:40:49 +0100

    Progress towards new database function

--- script.py
+++ script.py
@@ -6,8 +6,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
 
-def create_license(driver, email_address: str, password: str):
-    ten_second_wait = WebDriverWait(driver, 10)
+def log_in(driver, email_address: str, password: str) -> None:
     log_in_url = 'https://developer.vuforia.com/vui/auth/login'
     driver.get(log_in_url)
     email_address_input_element = driver.find_element_by_id('login_email')
@@ -16,6 +15,22 @@ def create_license(driver, email_address: str, password: str):
     password_input_element = driver.find_element_by_id('login_password')
     password_input_element.send_keys(password)
     password_input_element.send_keys(Keys.RETURN)
+
+    # This shows that the log in is complete.
+    ten_second_wait = WebDriverWait(driver, 10)
+
+    get_development_key_button_element = ten_second_wait.until(
+        expected_conditions.presence_of_element_located(
+            (By.ID, 'get-development-key'),
+        ),
+    )
+
+
+def create_license(driver) -> None:
+    licenses_url = 'https://developer.vuforia.com/vui/develop/licenses'
+    driver.get(licenses_url)
+
+    ten_second_wait = WebDriverWait(driver, 10)
 
     get_development_key_button_element = ten_second_wait.until(
         expected_conditions.presence_of_element_located(
@@ -39,15 +54,17 @@ def create_license(driver, email_address: str, password: str):
     agree_terms_checkbox_element.submit()
 
 
+def create_database(driver) -> None:
+    pass
+
+
 def main():
     driver = webdriver.Safari()
     email_address = os.environ['EMAIL_ADDRESS']
     password = os.environ['PASSWORD']
-    create_license(
-        driver=driver,
-        email_address=email_address,
-        password=password,
-    )
+    log_in(driver=driver, email_address=email_address, password=password)
+    create_license(driver=driver)
+    create_database(driver=driver)
     driver.close()
 
 
