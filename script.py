3990215a9dd6756bc7826389a4ca90a9e88093f4
Adam Dangoor <adamdangoor@gmail.com>
2020-06-10 10:33:13 +0100

    Move license creation into a function

--- script.py
+++ script.py
@@ -5,39 +5,51 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Safari()
-ten_second_wait = WebDriverWait(driver, 10)
-log_in_url = 'https://developer.vuforia.com/vui/auth/login'
-licenses_url = 'https://developer.vuforia.com/vui/develop/licenses'
-new_free_licenses_url = 'https://developer.vuforia.com/vui/develop/licenses/free/new'
-email_address = os.environ['EMAIL_ADDRESS']
-password = os.environ['PASSWORD']
-driver.get(log_in_url)
-email_address_input_element = driver.find_element_by_id('login_email')
-email_address_input_element.send_keys(email_address)
 
-password_input_element = driver.find_element_by_id('login_password')
-password_input_element.send_keys(password)
-password_input_element.send_keys(Keys.RETURN)
+def create_license(driver, email_address: str, password: str):
+    ten_second_wait = WebDriverWait(driver, 10)
+    log_in_url = 'https://developer.vuforia.com/vui/auth/login'
+    driver.get(log_in_url)
+    email_address_input_element = driver.find_element_by_id('login_email')
+    email_address_input_element.send_keys(email_address)
 
-get_development_key_button_element = ten_second_wait.until(
-    expected_conditions.presence_of_element_located(
-        (By.ID, 'get-development-key'),
-    ),
-)
+    password_input_element = driver.find_element_by_id('login_password')
+    password_input_element.send_keys(password)
+    password_input_element.send_keys(Keys.RETURN)
 
-get_development_key_button_element.click()
+    get_development_key_button_element = ten_second_wait.until(
+        expected_conditions.presence_of_element_located(
+            (By.ID, 'get-development-key'),
+        ),
+    )
 
-license_name_input_element = ten_second_wait.until(
-    expected_conditions.presence_of_element_located(
-        (By.ID, 'license-name'),
-    ),
-)
+    get_development_key_button_element.click()
 
-license_name = 'foo'
-license_name_input_element.send_keys(license_name)
+    license_name_input_element = ten_second_wait.until(
+        expected_conditions.presence_of_element_located(
+            (By.ID, 'license-name'),
+        ),
+    )
 
-agree_terms_checkbox_element = driver.find_element_by_id('agree-terms-checkbox')
-agree_terms_checkbox_element.submit()
-import pdb; pdb.set_trace()
-driver.close()
+    license_name = 'foo'
+    license_name_input_element.send_keys(license_name)
+
+    agree_terms_id = 'agree-terms-checkbox'
+    agree_terms_checkbox_element = driver.find_element_by_id(agree_terms_id)
+    agree_terms_checkbox_element.submit()
+
+
+def main():
+    driver = webdriver.Safari()
+    email_address = os.environ['EMAIL_ADDRESS']
+    password = os.environ['PASSWORD']
+    create_license(
+        driver=driver,
+        email_address=email_address,
+        password=password,
+    )
+    driver.close()
+
+
+if __name__ == '__main__':
+    main()
